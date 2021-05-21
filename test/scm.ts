87b525c5c2c322f28fd15784b1dd2759b91a6231
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-05-21 10:52:47 +0300

    Initialize contract in the setup hook instead of in each test

--- test/scm.ts
+++ test/scm.ts
@@ -2,14 +2,36 @@ import {expect} from "chai";
 import {ethers} from "hardhat";
 
 describe("SCM contract", function() {
-    it("should assign the total supply of tokens to the owner", async function() {
-        const [owner] = await ethers.getSigners();
+    const totalSupply = 100;
 
-        const Token = await ethers.getContractFactory("SCM");
+    let Scm;
+    let scm;
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
 
-        const hardhatToken = await Token.deploy(100);
+    beforeEach(async function () {
+        Scm = await ethers.getContractFactory("SCM");
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        scm = await Scm.deploy(totalSupply);
+    });
 
-        expect(await hardhatToken.totalSupply()).to.equal(100);
-        expect(await hardhatToken.balanceOf(owner.address)).to.equal(100);
+    describe("deploy", function () {
+        it("should provide basic information", async function() {
+            expect(await scm.name()).to.equal("Scam");
+            expect(await scm.symbol()).to.equal("SCM");
+            expect(await scm.decimals()).to.equal(18);
+        });
+
+        it("should provide the total supply of tokens", async function() {
+            expect(await scm.totalSupply()).to.equal(totalSupply);
+        });
+
+        it("should assign the total supply of tokens to the owner", async function() {
+            expect(await scm.balanceOf(owner.address)).to.equal(totalSupply);
+            expect(await scm.balanceOf(addr1.address)).to.equal(0);
+            expect(await scm.balanceOf(addr2.address)).to.equal(0);
+        });
     });
 });
