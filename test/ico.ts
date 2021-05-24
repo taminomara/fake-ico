edddd51e377cad4e65e982092bb8ee9cf71ca88c
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-05-24 16:59:39 +0300

    Add test file for ICO contract

---
+++ test/ico.ts
@@ -0,0 +1,27 @@
+import {expect} from "chai";
+import {ethers} from "hardhat";
+import {describe} from "mocha";
+
+describe("ICO contract", async () => {
+    let Eth;
+    let eth;
+    let Ico;
+    let ico;
+    let scm;
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+
+    beforeEach(async () => {
+        Eth = await ethers.getContractFactory("WETH9");
+        eth = await Eth.deploy();
+
+        Ico = await ethers.getContractFactory("ICO");
+        ico = await Ico.deploy(eth.address);
+
+        scm = ico.scm();
+
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    });
+});
