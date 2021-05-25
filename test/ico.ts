02517d2ad121ec3cf17088b626c183a6868facd4
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-05-25 13:36:56 +0300

    Some more tests for ICO

--- test/ico.ts
+++ test/ico.ts
@@ -24,24 +24,43 @@ describe("ICO contract", async () => {
 
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-        await eth.connect(addr1).deposit({value: 10});
-        await eth.connect(addr2).deposit({value: 10});
+        await eth.connect(owner).deposit({value: ethers.utils.parseEther("100")});
+        await eth.connect(owner).approve(ico.address, ethers.utils.parseEther("100"));
+        await eth.connect(addr1).deposit({value: ethers.utils.parseEther("100")});
+        await eth.connect(addr1).approve(ico.address, ethers.utils.parseEther("100"));
+        await eth.connect(addr2).deposit({value: ethers.utils.parseEther("100")});
+        await eth.connect(addr2).approve(ico.address, ethers.utils.parseEther("100"));
     });
 
     it("provides basic information", async () => {
+        expect(await ico.target()).to.equal(ethers.utils.parseEther("10"));
+        expect(await ico.rate()).to.equal(10);
+        expect(await ico.holdDuration()).to.equal(2 * 60);
     });
 
     describe("state method", async () => {
         it("says state is ongoing when we don't have enough funds", async () => {
-
+            expect(await ico.state())
+                .to.equal(0);
         });
 
         it("says state is closed when we have enough funds", async () => {
+            await expect(ico.fund(ethers.utils.parseEther("10")))
+                .to.not.be.reverted;
 
+            expect(await ico.state())
+                .to.equal(1);
         });
 
         it("says state is finished when we have enough funds and enough time has passed", async () => {
+            await expect(ico.fund(ethers.utils.parseEther("10")))
+                .to.not.be.reverted;
 
+            await ethers.provider.send("evm_increaseTime", [2 * 60]);
+            await ethers.provider.send("evm_mine", []);
+
+            expect(await ico.state())
+                .to.equal(2);
         });
     });
 
