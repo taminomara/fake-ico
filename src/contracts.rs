456af2e258067687f051c1a2ba6dfea13c04529f
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-06-02 12:15:04 +0300

    Code to deploy ICO

--- src/contracts.rs
+++ src/contracts.rs
@@ -1,3 +1,3 @@
-ethcontract::contract!(
-    pub "artifacts/contracts/ico.sol/ICO.json"
-);
+ethcontract::contract!(pub "artifacts/contracts/ico.sol/ICO.json");
+ethcontract::contract!(pub "artifacts/contracts/scm.sol/SCM.json");
+ethcontract::contract!(pub "artifacts/canonical-weth/contracts/WETH9.sol/WETH9.json");
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,112 @@
-use ethcontract::transport::DynTransport;
-use clap::ArgMatches;
+use ethcontract::prelude::*;
 
 mod contracts;
 
-async fn create_transport(schema: &str) -> Result<DynTransport, web3::Error> {
-    use web3::transports::{Http, Ipc};
-    match schema.split_once("://") {
-        Some(("http", _)) => Http::new(schema).map(DynTransport::new),
-        Some(("https", _)) => Http::new(schema).map(DynTransport::new),
-        Some(("ipc", path)) => Ipc::new(path).await.map(DynTransport::new),
-        _ => Err(web3::Error::Transport("invalid transport schema".into())),
-    }
-}
-
 #[tokio::main]
-async fn main() -> web3::Result<()> {
+async fn main() -> Result<(), ethcontract::errors::DeployError> {
     let app = clap::App::new("Scam-ICO")
         .about("Use CLI to spend your precious ETH and get some 💩!")
+        .setting(clap::AppSettings::SubcommandRequiredElseHelp)
         .arg(
-            clap::Arg::with_name("transport")
-                .long("transport")
-                .short("t")
-                .required(true)
+            clap::Arg::with_name("host")
+                .long("host")
+                .short("h")
                 .takes_value(true)
-                .value_name("transport")
-                .help(
-                    "transport that's used to connect to an ethereum node; \
-                    we support 'https://' and 'ipc://'",
-                ),
+                .value_name("url")
+                .default_value("http://localhost:8545")
+                .help("endpoint for ethereum node"),
         )
         .subcommand(
             clap::SubCommand::with_name("deploy")
                 .about("deploy a new ICO contract")
+                .arg(
+                    clap::Arg::with_name("weth")
+                        .long("weth")
+                        .takes_value(true)
+                        .value_name("address")
+                        .help("address of the WETH contract"),
+                )
+                .arg(
+                    clap::Arg::with_name("deploy-weth")
+                        .long("deploy-weth")
+                        .conflicts_with("weth")
+                        .help("deploy new weth contract"),
+                ),
         );
 
     let matches = app.get_matches();
 
-    let transport = create_transport(matches.value_of("transport").unwrap()).await?;
-    let web3 = web3::Web3::new(transport);
+    let account = std::env::var("ETH_ACCOUNT")
+        .expect("environment variable ETH_ACCOUNT must be present")
+        .parse()
+        .expect("environment variable ETH_ACCOUNT must contain valid address");
 
-    // match matches.subcommand() {
-    //     ("deploy",  Some(sub_m)) => {},
-    //     (_, _) => {}
-    // }
-    //
-    // println!("Calling accounts.");
-    // let accounts = web3.eth().accounts().await?;
-    // println!("Accounts: {:?}", accounts);
+    let password =
+        std::env::var("ETH_PASSWORD").expect("environment variable ETH_PASSWORD must be present");
 
-    // use contracts::ICO;
-    // let c = contracts::ICO::deployed();
+    let transport = Http::new(matches.value_of("host").unwrap()).expect("http connection failed");
+    let web3 = Web3::new(transport);
 
-    contracts::ICO::builder(&web3, "".parse().unwrap(), "".parse().unwrap());
+    web3.personal()
+        .unlock_account(account, &password, None)
+        .await?;
 
-    // println!("Calling accounts.");
-    // let mut accounts = web3.eth().accounts().await?;
-    // println!("Accounts: {:?}", accounts);
+    match matches.subcommand() {
+        ("deploy", Some(sub_m)) => deploy(&web3, sub_m, account).await,
+        (cmd, _) => panic!("unknown command {:?}", cmd),
+    }
+}
 
-    //ipc:///Users/taminomara/Documents/eth/data/geth.ipc
-    // println!("Calling balance.");
-    // let balance = web3.eth().balance("00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap(), None).await?;
-    // println!("Balance: {}", balance);
+async fn deploy(
+    web3: &Web3<Http>,
+    args: &clap::ArgMatches<'static>,
+    address: Address
+) -> Result<(), ethcontract::errors::DeployError> {
+    let weth = {
+        if args.is_present("deploy-weth") {
+            contracts::WETH9::builder(&web3).deploy().await?.address()
+        } else if let Some(address) = args.value_of("weth") {
+            address
+                .parse()
+                .expect("invalid address for --weth argument")
+        } else {
+            match web3.eth().chain_id().await?.as_u64() {
+                // Mainnet
+                1 => "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+                    .parse()
+                    .unwrap(),
+
+                // Ropsten
+                3 => "0xc778417e063141139fce010982780140aa0cd5ab"
+                    .parse()
+                    .unwrap(),
+
+                // Rinkeby
+                4 => "0xc778417e063141139fce010982780140aa0cd5ab"
+                    .parse()
+                    .unwrap(),
+
+                // Kovan
+                42 => "0xd0a1e359811322d97991e03f863a0c30c2cf029c"
+                    .parse()
+                    .unwrap(),
+
+                id => panic!(
+                    "unknown canonical WETH address for net id {}; \
+                    please specify WETH contract manually with --weth flag",
+                    id
+                ),
+            }
+        }
+    };
+
+    println!("Deploying ICO...");
+    println!("Using WETH implementation at {:?}", weth);
+    println!("ICO will send ether to {:?}", address);
+
+    let ico = contracts::ICO::builder(&web3, weth, address).deploy().await?;
+
+    println!("ICO deployed at {:?}", ico.address());
 
     Ok(())
 }
