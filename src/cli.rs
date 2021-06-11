1ca95fc57ba093db666696800370bb5a2b900437
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-06-11 12:14:52 +0300

    Introduce SCM currency

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,12 @@
 use ethcontract::prelude::*;
 use std::str::FromStr;
 
+/// Trait for CLI arguments that represent an amount of some currency.
+pub trait Currency: FromStr {
+    /// Get the underlying int type.
+    fn as_inner(&self) -> U256;
+}
+
 /// For CLI arguments that take amount of ether.
 ///
 /// Supports parsing ether values in human-readable form,
@@ -8,9 +14,8 @@ use std::str::FromStr;
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct Eth(U256);
 
-impl Eth {
-    /// Get the underlying int type.
-    pub fn as_inner(self) -> U256 {
+impl Currency for Eth {
+    fn as_inner(&self) -> U256 {
         self.0
     }
 }
@@ -38,7 +43,7 @@ impl FromStr for Eth {
         } else if let Some(s) = s.strip_suffix("wei") {
             (s, U256::exp10(0))
         } else {
-            (s.as_str(), U256::exp10(18))
+            (s.as_str(), U256::exp10(0))
         };
 
         if s.starts_with("0x") {
@@ -50,16 +55,16 @@ impl FromStr for Eth {
 }
 
 #[cfg(test)]
-mod test {
+mod test_eth {
     use super::*;
 
     #[test]
     fn eth_from_str() -> Result<(), Box<dyn std::error::Error>> {
         assert_eq!(Eth::from_str("0")?.0, U256::from(0));
-        assert_eq!(Eth::from_str("1")?.0, U256::from_dec_str("1000000000000000000")?);
-        assert_eq!(Eth::from_str("15")?.0, U256::from_dec_str("15000000000000000000")?);
-        assert_eq!(Eth::from_str("0x1")?.0, U256::from_dec_str("1000000000000000000")?);
-        assert_eq!(Eth::from_str("0x15")?.0, U256::from_dec_str("21000000000000000000")?);
+        assert_eq!(Eth::from_str("1")?.0, U256::from_dec_str("1")?);
+        assert_eq!(Eth::from_str("15")?.0, U256::from_dec_str("15")?);
+        assert_eq!(Eth::from_str("0x1")?.0, U256::from_dec_str("1")?);
+        assert_eq!(Eth::from_str("0x15")?.0, U256::from_dec_str("21")?);
 
         assert_eq!(Eth::from_str("5wei")?.0, U256::from_dec_str("5")?);
         assert_eq!(Eth::from_str("5kwei")?.0, U256::from_dec_str("5000")?);
@@ -69,6 +74,78 @@ mod test {
         assert_eq!(Eth::from_str("5pwei")?.0, U256::from_dec_str("5000000000000000")?);
         assert_eq!(Eth::from_str("5eth")?.0, U256::from_dec_str("5000000000000000000")?);
         assert_eq!(Eth::from_str("5ether")?.0, U256::from_dec_str("5000000000000000000")?);
+
+        Ok(())
+    }
+}
+
+/// For CLI arguments that take amount of SCM tokens.
+///
+/// Supports parsing ether values in human-readable form,
+/// i.e. `1scm` or `100asc` or others.
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub struct Scm(U256);
+
+impl Currency for Scm {
+    fn as_inner(&self) -> U256 {
+        self.0
+    }
+}
+
+impl FromStr for Scm {
+    type Err = uint::FromStrRadixErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.to_ascii_lowercase();
+
+        let (s, modifier) = if let Some(s) = s.strip_suffix("scam") {
+            (s, U256::exp10(18))
+        } else if let Some(s) = s.strip_suffix("scm") {
+            (s, U256::exp10(18))
+        } else if let Some(s) = s.strip_suffix("msc") {
+            (s, U256::exp10(15))
+        } else if let Some(s) = s.strip_suffix("usc") {
+            (s, U256::exp10(12))
+        } else if let Some(s) = s.strip_suffix("nsc") {
+            (s, U256::exp10(9))
+        } else if let Some(s) = s.strip_suffix("psc") {
+            (s, U256::exp10(6))
+        } else if let Some(s) = s.strip_suffix("fsc") {
+            (s, U256::exp10(3))
+        } else if let Some(s) = s.strip_suffix("asc") {
+            (s, U256::exp10(0))
+        } else {
+            (s.as_str(), U256::exp10(0))
+        };
+
+        if s.starts_with("0x") {
+            Ok(Scm(U256::from_str_radix(s, 16)? * modifier))
+        } else {
+            Ok(Scm(U256::from_str_radix(s, 10)? * modifier))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_scm {
+    use super::*;
+
+    #[test]
+    fn eth_from_str() -> Result<(), Box<dyn std::error::Error>> {
+        assert_eq!(Scm::from_str("0")?.0, U256::from(0));
+        assert_eq!(Scm::from_str("1")?.0, U256::from_dec_str("1")?);
+        assert_eq!(Scm::from_str("15")?.0, U256::from_dec_str("15")?);
+        assert_eq!(Scm::from_str("0x1")?.0, U256::from_dec_str("1")?);
+        assert_eq!(Scm::from_str("0x15")?.0, U256::from_dec_str("21")?);
+
+        assert_eq!(Scm::from_str("5asc")?.0, U256::from_dec_str("5")?);
+        assert_eq!(Scm::from_str("5fsc")?.0, U256::from_dec_str("5000")?);
+        assert_eq!(Scm::from_str("5psc")?.0, U256::from_dec_str("5000000")?);
+        assert_eq!(Scm::from_str("5nsc")?.0, U256::from_dec_str("5000000000")?);
+        assert_eq!(Scm::from_str("5usc")?.0, U256::from_dec_str("5000000000000")?);
+        assert_eq!(Scm::from_str("5msc")?.0, U256::from_dec_str("5000000000000000")?);
+        assert_eq!(Scm::from_str("5scm")?.0, U256::from_dec_str("5000000000000000000")?);
+        assert_eq!(Scm::from_str("5scam")?.0, U256::from_dec_str("5000000000000000000")?);
 
         Ok(())
     }
--- src/erc20.rs
+++ src/erc20.rs
@@ -1,191 +1,214 @@
 use ethcontract::prelude::*;
 
-use crate::cli::Eth;
+use crate::cli::{Currency, Eth, Scm};
 
 #[derive(structopt::StructOpt)]
 #[structopt(about = "Manage SCM tokens")]
 pub enum ScmCommand {
-    Balance(Balance),
-    Transfer(Transfer),
-    Allowance(Allowance),
-    Approve(Approve),
+    #[structopt(about = "Get balance of the given wallet")]
+    Balance {
+        #[structopt(help = "Account we're fetching balance for (uses your account by default)")]
+        address: Option<Address>,
+    },
+
+    #[structopt(about = "Transfer funds between accounts")]
+    Transfer {
+        #[structopt(help = "Where are we transferring funds to")]
+        recipient: Address,
+        #[structopt(help = "Amount of funds we are transferring")]
+        funds: Scm,
+        #[structopt(
+            long,
+            help = "Where are we transferring funds from (uses your account by default)"
+        )]
+        owner: Option<Address>,
+    },
+
+    #[structopt(about = "Check allowance for the given owner-spender pair")]
+    Allowance {
+        #[structopt(help = "Who owns tokens")]
+        owner: Address,
+        #[structopt(help = "Who will be allowed to spend tokens")]
+        spender: Address,
+    },
+
+    #[structopt(about = "Allow some other user to withdraw funds from your account")]
+    Approve {
+        #[structopt(help = "Who is allowed to withdraw funds")]
+        spender: Address,
+        #[structopt(
+            help = "Amount of funds they are allowed to withdraw (overrides previous allowance)"
+        )]
+        value: Scm,
+    },
 }
 
 impl ScmCommand {
     pub async fn invoke(&self, account: Account, web3: &Web3<Http>) {
-        let address = crate::contracts::get_scm_address(web3).await;
-        let contract = crate::contracts::IERC20::at(web3, address);
+        let contract_address = crate::contracts::get_scm_address(web3).await;
+        let contract = crate::contracts::SCM::at(web3, contract_address);
 
         match self {
-            Self::Balance(command) => command.invoke(account, &contract).await,
-            Self::Transfer(command) => command.invoke(account, &contract).await,
-            Self::Allowance(command) => command.invoke(account, &contract).await,
-            Self::Approve(command) => command.invoke(account, &contract).await,
+            Self::Balance { address } => {
+                let balance = contract
+                    .balance_of(address.unwrap_or(account.address()))
+                    .from(account.address())
+                    .call()
+                    .await
+                    .expect("balance fetch failed");
+
+                println!("{}asc", balance);
+            }
+
+            Self::Transfer { recipient, funds, owner } => {
+                contract
+                    .transfer_from(owner.unwrap_or(account.address()), *recipient, funds.as_inner())
+                    .from(account)
+                    .call()
+                    .await
+                    .expect("transfer failed");
+            }
+
+            Self::Allowance { owner, spender } => {
+                let allowance = contract
+                    .allowance(*owner, *spender)
+                    .from(account.address())
+                    .call()
+                    .await
+                    .expect("allowance fetch failed");
+
+                println!("{}asc", allowance);
+            }
+
+            Self::Approve { spender, value } => {
+                contract
+                    .approve(*spender, value.as_inner())
+                    .from(account)
+                    .call()
+                    .await
+                    .expect("approve failed");
+            }
         }
     }
 }
 
 #[derive(structopt::StructOpt)]
 #[structopt(about = "Manage wrapped ethereum tokens")]
-pub enum WethCommand {
-    Balance(Balance),
-    Transfer(Transfer),
-    Allowance(Allowance),
-    Approve(Approve),
-    Deposit(Deposit),
-    Withdraw(Withdraw),
+pub enum EthCommand {
+    #[structopt(about = "Get balance of the given wallet")]
+    Balance {
+        #[structopt(help = "Account we're fetching balance for (uses your account by default)")]
+        address: Option<Address>,
+    },
+
+    #[structopt(about = "Transfer funds between accounts")]
+    Transfer {
+        #[structopt(help = "Where are we transferring funds to")]
+        recipient: Address,
+        #[structopt(help = "Amount of funds we are transferring")]
+        funds: Eth,
+        #[structopt(
+            long,
+            help = "Where are we transferring funds from (uses your account by default)"
+        )]
+        owner: Option<Address>,
+    },
+
+    #[structopt(about = "Check allowance for the given owner-spender pair")]
+    Allowance {
+        #[structopt(help = "Who owns tokens")]
+        owner: Address,
+        #[structopt(help = "Who will be allowed to spend tokens")]
+        spender: Address,
+    },
+
+    #[structopt(about = "Allow some other user to withdraw funds from your account")]
+    Approve {
+        #[structopt(help = "Who is allowed to withdraw funds")]
+        spender: Address,
+        #[structopt(
+            help = "Amount of funds they are allowed to withdraw (overrides previous allowance)"
+        )]
+        value: Eth,
+    },
+
+    #[structopt(about = "Wrap ether")]
+    Deposit {
+        #[structopt(help = "Amount of ether to wrap")]
+        amount: Eth,
+    },
+
+    #[structopt(about = "Unwrap ether")]
+    Withdraw {
+        #[structopt(help = "Amount of ether to unwrap")]
+        amount: Eth,
+    },
 }
 
-impl WethCommand {
+impl EthCommand {
     pub async fn invoke(&self, account: Account, web3: &Web3<Http>) {
-        let address = crate::contracts::get_weth_address(web3).await;
-        let contract = crate::contracts::IERC20::at(web3, address);
+        let contract_address = crate::contracts::get_scm_address(web3).await;
+        let contract = crate::contracts::WETH9::at(web3, contract_address);
 
         match self {
-            Self::Balance(command) => command.invoke(account, &contract).await,
-            Self::Transfer(command) => command.invoke(account, &contract).await,
-            Self::Allowance(command) => command.invoke(account, &contract).await,
-            Self::Approve(command) => command.invoke(account, &contract).await,
-            Self::Deposit(command) => {
-                command
-                    .invoke(account, &crate::contracts::WETH9::at(web3, address))
+            Self::Balance { address } => {
+                let balance = contract
+                    .balance_of(address.unwrap_or(account.address()))
+                    .from(account.address())
+                    .call()
                     .await
+                    .expect("balance fetch failed");
+
+                println!("{}wei", balance);
             }
-            Self::Withdraw(command) => {
-                command
-                    .invoke(account, &crate::contracts::WETH9::at(web3, address))
+
+            Self::Transfer { recipient, funds, owner } => {
+                contract
+                    .transfer_from(owner.unwrap_or(account.address()), *recipient, funds.as_inner())
+                    .from(account)
+                    .call()
                     .await
+                    .expect("transfer failed");
+            }
+
+            Self::Allowance { owner, spender } => {
+                let allowance = contract
+                    .allowance(*owner, *spender)
+                    .from(account.address())
+                    .call()
+                    .await
+                    .expect("allowance fetch failed");
+
+                println!("{}wei", allowance);
+            }
+
+            Self::Approve { spender, value } => {
+                contract
+                    .approve(*spender, value.as_inner())
+                    .from(account)
+                    .call()
+                    .await
+                    .expect("approve failed");
+            }
+
+            Self::Deposit { amount } => {
+                contract
+                    .deposit()
+                    .from(account)
+                    .value(amount.as_inner())
+                    .call()
+                    .await
+                    .expect("deposit failed");
+            }
+
+            Self::Withdraw { amount } => {
+                contract
+                    .withdraw(amount.as_inner())
+                    .from(account)
+                    .call()
+                    .await
+                    .expect("withdraw failed");
             }
         }
-    }
-}
-
-#[derive(structopt::StructOpt)]
-#[structopt(about = "Get balance of the given wallet")]
-pub struct Balance {
-    #[structopt(help = "Account we're fetching balance for (uses your account by default)")]
-    pub address: Option<Address>,
-}
-
-impl Balance {
-    pub async fn invoke(&self, account: Account, contract: &crate::contracts::IERC20) {
-        let address = self.address.unwrap_or_else(|| account.address());
-
-        let balance = contract
-            .balance_of(address)
-            .from(account.address())
-            .call()
-            .await
-            .expect("balance fetch failed");
-
-        println!("{}", balance);
-    }
-}
-
-#[derive(structopt::StructOpt)]
-#[structopt(about = "Transfer funds between accounts")]
-pub struct Transfer {
-    #[structopt(help = "Where are we transferring funds to")]
-    pub recipient: Address,
-    #[structopt(help = "Amount of funds we are transferring")]
-    pub funds: Eth,
-    #[structopt(
-        long,
-        help = "Where are we transferring funds from (uses your account by default)"
-    )]
-    pub owner: Option<Address>,
-}
-
-impl Transfer {
-    pub async fn invoke(&self, account: Account, contract: &crate::contracts::IERC20) {
-        let owner = self.owner.unwrap_or_else(|| account.address());
-
-        contract
-            .transfer_from(owner, self.recipient, self.funds.as_inner())
-            .from(account)
-            .call()
-            .await
-            .expect("transfer failed");
-    }
-}
-
-#[derive(structopt::StructOpt)]
-#[structopt(about = "Check allowance for the given owner-spender pair")]
-pub struct Allowance {
-    #[structopt(help = "Who owns tokens")]
-    pub owner: Address,
-    #[structopt(help = "Who will be allowed to spend tokens")]
-    pub spender: Address,
-}
-
-impl Allowance {
-    pub async fn invoke(&self, account: Account, contract: &crate::contracts::IERC20) {
-        let allowance = contract
-            .allowance(self.owner, self.spender)
-            .from(account.address())
-            .call()
-            .await
-            .expect("allowance fetch failed");
-
-        println!("{}", allowance);
-    }
-}
-
-#[derive(structopt::StructOpt)]
-#[structopt(about = "Allow some other user to withdraw funds from your account")]
-pub struct Approve {
-    #[structopt(help = "Who is allowed to withdraw funds")]
-    pub spender: Address,
-    #[structopt(
-        help = "Amount of funds they are allowed to withdraw (overrides previous allowance)"
-    )]
-    pub value: Eth,
-}
-
-impl Approve {
-    pub async fn invoke(&self, account: Account, contract: &crate::contracts::IERC20) {
-        contract
-            .approve(self.spender, self.value.as_inner())
-            .from(account)
-            .call()
-            .await
-            .expect("approve failed");
-    }
-}
-
-#[derive(structopt::StructOpt)]
-#[structopt(about = "Wrap ether")]
-pub struct Deposit {
-    #[structopt(help = "Amount of ether to wrap")]
-    pub amount: Eth,
-}
-
-impl Deposit {
-    pub async fn invoke(&self, account: Account, contract: &crate::contracts::WETH9) {
-        contract
-            .deposit()
-            .from(account)
-            .value(self.amount.as_inner())
-            .call()
-            .await
-            .expect("deposit failed");
-    }
-}
-
-#[derive(structopt::StructOpt)]
-#[structopt(about = "Unwrap ether")]
-pub struct Withdraw {
-    #[structopt(help = "Amount of ether to unwrap")]
-    pub amount: Eth,
-}
-
-impl Withdraw {
-    pub async fn invoke(&self, account: Account, contract: &crate::contracts::WETH9) {
-        contract
-            .withdraw(self.amount.as_inner())
-            .from(account)
-            .call()
-            .await
-            .expect("withdraw failed");
     }
 }
