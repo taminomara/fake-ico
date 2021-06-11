7a4e4b975588d464637aa50adbfd8b6a5b13d632
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-06-11 17:09:23 +0300

    Proper deploy script

--- deploy/00_init.ts
+++ deploy/00_init.ts
@@ -1,11 +1,18 @@
 module.exports = async ({getNamedAccounts, deployments, getChainId, getUnnamedAccounts}) => {
     const {deployer} = await getNamedAccounts();
 
-    // const weth = await deployments.get("WETH9");
+    const weth = await deployments.get("WETH9");
 
-    await deployments.deploy('SCM', {
+    const ico = await deployments.deploy('ICO', {
         from: deployer,
         gasLimit: 4000000,
-        args: [1000],
+        args: [weth.address, deployer],
+    });
+
+    const scmAddress = await deployments.read('ICO', {}, 'scm');
+    const scm = await deployments.getArtifact('SCM');
+    deployments.save('SCM', {
+        abi: scm.abi,
+        address: scmAddress,
     });
 };
