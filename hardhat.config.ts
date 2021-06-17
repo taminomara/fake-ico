fcad7794a83b55258a7b11c43c687c143eea1ed2
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-06-17 15:04:25 +0300

    Add config for non-local chains

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import "hardhat-deploy";
 
 import {HttpNetworkUserConfig} from "hardhat/types";
 
-const {MNEMONIC, PK} = process.env;
+const {MNEMONIC, PK, INFURA_KEY} = process.env;
 
 const DEFAULT_MNEMONIC =
     "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
@@ -25,6 +25,26 @@ module.exports = {
             ...sharedNetworkConfig,
             live: false,
             saveDeployments: true,
+        },
+        mainnet: {
+            ...sharedNetworkConfig,
+            url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+        },
+        rinkeby: {
+            ...sharedNetworkConfig,
+            url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+        },
+        goerli: {
+            ...sharedNetworkConfig,
+            url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+        },
+        ropsten: {
+            ...sharedNetworkConfig,
+            url: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
+        },
+        kovan: {
+            ...sharedNetworkConfig,
+            url: `https://kovan.infura.io/v3/${INFURA_KEY}`,
         },
     },
 
